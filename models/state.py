360fcb9f0f28237055cfd1e2707ed5978cd75e75
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-01-05 11:37:59 +0200

    Feat: Update the engine storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship 
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+    
+    """ Give the file storage its meaning """
+    if getenv(HBNB_TYPE_STORAGE) != "db":
+        @property
+        def cities(self):
+            """ Get the list of all City objects """
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state.id == self.id:
+                    city_list.append(city)
+            return city_list
