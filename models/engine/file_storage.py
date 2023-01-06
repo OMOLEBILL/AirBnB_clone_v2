57d214e2b0170c6f8af5e66d5d21821bfeab2af2
OMOLEBILL <omolebill01@gmail.com>
2023-01-06 21:23:56 +0000

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,34 +31,21 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(self.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = self.clsdict[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
@@ -66,4 +53,10 @@ class FileStorage:
         """to delete obj from __objects if it's inside"""
         D = FileStorage.__objects
         ob = f"{type(obj).__name__}.{obj.id}"
-        self.__objects.pop(ob, None)
+        if ob in self.__objects:
+            del self.__objects[ob]
+
+    def close(self):
+        """deserializing the JSON file to objects
+        """
+        self.reload()
