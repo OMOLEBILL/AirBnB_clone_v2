95e16a97bf3a916da44b80fe7efcb3de9e2b0c10
OMOLEBILL <omolebill01@gmail.com>
2023-01-06 15:54:44 +0000

    stilltyring to fix it

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,13 @@
     instantiates a database storage engine(DBStorage.)
 - Otherwise, instantiates a fule storage engine (FileStorage).
 """
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from os import getenv
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
