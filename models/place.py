1e6a8049bcc2013cc7c642b926a1eeeab15f442a
OMOLEBILL <omolebill01@gmail.com>
2023-01-06 21:07:58 +0000

    bugfix

--- models/place.py
+++ models/place.py
@@ -6,13 +6,14 @@ from sqlalchemy.orm import relationship
 import models
 from os import getenv
 
-
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey("places.id"), primary_key=True),
-    Column('amenity_id', String(60), ForeignKey("amenities.id"),
-           primary_key=True)
-)
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    place_amenity = Table(
+        'place_amenity', Base.metadata,
+        Column('place_id', String(60), ForeignKey("places.id"),
+               primary_key=True),
+        Column('amenity_id', String(60), ForeignKey("amenities.id"),
+               primary_key=True)
+    )
 
 
 class Place(BaseModel, Base):
