13f15d6517a8d38e454ec1d7fb1e217cda23a726
OMOLEBILL <omolebill01@gmail.com>
2023-01-23 22:33:12 +0000

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of the web_static
+#we intsall NGinx and set up directorios
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+# Install Nginx if not already installed
+if ! [ -x "$(command -v nginx)" ]; then
+  apt-get update
+  apt-get install -y nginx
+fi
+
+# Create necessary directories
+mkdir -p /data/web_static/releases/test/
+
+# Create fake HTML file
+echo "Hello, World!" | sudo tee /data/web_static/releases/test/index.html
+
+# Create and update symbolic link
+if [ -L "/data/web_static/current" ]; then
+  rm /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of /data/ to ubuntu user and group
+chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+sudo sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Restart Nginx
+sudo service nginx restart
