6a25d32493f944010bdadb0a6a620d7e063254e6
KitwanaSh <rodriguekitweze@gmail.com>
2023-01-23 19:59:47 +0200

    Feat: add the index file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# sets up the web servers for the deployment of the web_static
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
