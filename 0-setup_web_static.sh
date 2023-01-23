a1aa05eedcae44264d696868422769c7d4111d69
OMOLEBILL <omolebill01@gmail.com>
2023-01-23 22:36:26 +0000

    sudomode

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,12 @@
 
 # Install Nginx if not already installed
 if ! [ -x "$(command -v nginx)" ]; then
-  apt-get update
-  apt-get install -y nginx
+  sudo apt-get update
+  sudo apt-get install -y nginx
 fi
 
 # Create necessary directories
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/
 
 # Create fake HTML file
 echo "Hello, World!" | sudo tee /data/web_static/releases/test/index.html
@@ -17,10 +17,10 @@ echo "Hello, World!" | sudo tee /data/web_static/releases/test/index.html
 if [ -L "/data/web_static/current" ]; then
   rm /data/web_static/current
 fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of /data/ to ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 sudo sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
