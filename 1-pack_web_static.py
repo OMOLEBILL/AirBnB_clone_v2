32df5e9bdb4d5ded1f1a42f05721d6179236913d
OMOLEBILL <omolebill01@gmail.com>
2023-01-24 00:04:05 +0000

    add

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module uses Fabric to craete an archive"""
+
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    """We create a folder @version then store the archive """
+    date_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(date_str)
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+    local("tar -cvzf versions/{} AirBnB_clone_v2/web_static".format(file_name))
+    return "versions/{}".format(file_name) \
+           if local("ls versions/{}".format(file_name)).succeeded else None
